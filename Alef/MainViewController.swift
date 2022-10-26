72d80acefb4b8d21319aa5fda4c94cd1f37deffa
Kostya Andreev <daewon@yandex.ru>
2022-10-26 11:54:03 +0400

    Code refactoring

--- Alef/MainViewController.swift
+++ Alef/MainViewController.swift
@@ -7,32 +7,61 @@
 
 import UIKit
 
-class MainViewController: UIViewController {
-
+final class MainViewController: UIViewController {
+    
+    lazy var personSectionStackView: UIStackView = {
+        let persanalDataStackView = UIStackView()
+        persanalDataStackView.translatesAutoresizingMaskIntoConstraints = false
+        persanalDataStackView.axis = .vertical
+        persanalDataStackView.alignment = .fill
+        persanalDataStackView.distribution = .fillEqually
+        persanalDataStackView.spacing = 6
+        persanalDataStackView.layer.borderColor = UIColor.lightGray.cgColor
+        persanalDataStackView.isLayoutMarginsRelativeArrangement = true
+        return persanalDataStackView
+    }()
+    
+    lazy var sectionHeader: UILabel = {
+        let headerLabel = UILabel()
+        
+        headerLabel.textColor = .black
+        headerLabel.font = UIFont.systemFont(ofSize: 16, weight: .bold)
+        headerLabel.translatesAutoresizingMaskIntoConstraints = false
+        
+        return headerLabel
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        let textFieldWithTitle = TextFieldWithTitle(with: "Name")
+        
+        let textFieldName = TextFieldWithTitle(with: "Name")
+        let textFieldAge = TextFieldWithTitle(with: "Age")
+        sectionHeader.text = "Персональные данные"
         
         view.backgroundColor = .white
+        view.addSubview(personSectionStackView)
         
-        view.addSubview(textFieldWithTitle)
-        
-        textFieldWithTitle.translatesAutoresizingMaskIntoConstraints = false
-        
-        NSLayoutConstraint.activate([
-            textFieldWithTitle.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 16),
-            textFieldWithTitle.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
-            textFieldWithTitle.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16),
-            textFieldWithTitle.heightAnchor.constraint(equalToConstant: 60)
-        ])
-        
+        addSubviews(to: personSectionStackView, with: sectionHeader, textFieldName,textFieldAge)
+        setupConstraints()
+    }
+}
+
+//MARK: - Private functions
+
+extension MainViewController {
+    
+    private func addSubviews(to stackView: UIStackView, with views: UIView...) {
+        views.forEach {
+            stackView.addArrangedSubview($0)
+        }
     }
     
-    private func addSubviews(views: UIView...) {
-        views.forEach {
-            view.addSubview($0)
-        }
+    private func setupConstraints() {
+        NSLayoutConstraint.activate([
+            personSectionStackView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 16),
+            personSectionStackView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
+            personSectionStackView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16),
+            personSectionStackView.heightAnchor.constraint(equalToConstant: 150)
+        ])
     }
 }
--- Alef/TextFieldWithTitle.swift
+++ Alef/TextFieldWithTitle.swift
@@ -7,62 +7,64 @@
 
 import UIKit
 
-class TextFieldWithTitle: UIView {
+final class TextFieldWithTitle: UIView {
 
-    lazy var textFieldHeaderLabel: UILabel = {
-        let headerLabel = UILabel()
+    lazy var textFieldTitle: UILabel = {
+        let textFieldTitle = UILabel()
         
-        headerLabel.textColor = .lightGray
-        headerLabel.font = UIFont.systemFont(ofSize: 12, weight: .bold)
-        headerLabel.translatesAutoresizingMaskIntoConstraints = false
+        textFieldTitle.textColor = .lightGray
+        textFieldTitle.font = UIFont.systemFont(ofSize: 12, weight: .bold)
+        textFieldTitle.translatesAutoresizingMaskIntoConstraints = false
         
-        return headerLabel
+        return textFieldTitle
     }()
     
-    lazy var personDataTextField: UITextField = {
-        let personDataTextField = UITextField()
+    lazy var dataTextField: UITextField = {
+        let dataTextField = UITextField()
         
-        personDataTextField.translatesAutoresizingMaskIntoConstraints = false
-
-        return personDataTextField
+        dataTextField.translatesAutoresizingMaskIntoConstraints = false
+        if let title = textFieldTitle.text {
+            dataTextField.placeholder = "Type your \(title) here..."
+        } else {
+            dataTextField.placeholder = "Type text here..."
+        }
+        
+        
+        return dataTextField
     }()
     
-    lazy var persanalDataStackView: UIStackView = {
-        let persanalDataStackView = UIStackView()
-        persanalDataStackView.translatesAutoresizingMaskIntoConstraints = false
-        persanalDataStackView.axis = .vertical
-        persanalDataStackView.alignment = .fill
-        persanalDataStackView.distribution = .fill
-        persanalDataStackView.spacing = 2
-        persanalDataStackView.layer.cornerRadius = 5
-        persanalDataStackView.layer.borderWidth = 1
-        persanalDataStackView.layer.borderColor = UIColor.lightGray.cgColor
-        persanalDataStackView.isLayoutMarginsRelativeArrangement = true
-        persanalDataStackView.layoutMargins = .init(top: 5, left: 10, bottom: 5, right: 10)
-        return persanalDataStackView
+    lazy var textFieldStackView: UIStackView = {
+        let textFieldStackView = UIStackView()
+        textFieldStackView.translatesAutoresizingMaskIntoConstraints = false
+        textFieldStackView.axis = .vertical
+        textFieldStackView.alignment = .fill
+        textFieldStackView.distribution = .fill
+        textFieldStackView.spacing = 2
+        textFieldStackView.layer.cornerRadius = 5
+        textFieldStackView.layer.borderWidth = 1
+        textFieldStackView.layer.borderColor = UIColor.lightGray.cgColor
+        textFieldStackView.isLayoutMarginsRelativeArrangement = true
+        textFieldStackView.layoutMargins = .init(top: 5, left: 10, bottom: 5, right: 10)
+        return textFieldStackView
     }()
     
-    override init(frame: CGRect) {
-        super.init(frame: frame)
-
-        print("UIView init")
-    }
-    
-    init(with label: String) {
+    init(with title: String) {
         
         super.init(frame: .zero)
-        textFieldHeaderLabel.text = label
-        persanalDataStackView.addArrangedSubview(textFieldHeaderLabel)
-        persanalDataStackView.addArrangedSubview(personDataTextField)
-        addSubview(persanalDataStackView)
+        
+        textFieldTitle.text = title
+        textFieldStackView.addArrangedSubview(textFieldTitle)
+        textFieldStackView.addArrangedSubview(dataTextField)
+        addSubview(textFieldStackView)
+        
+        translatesAutoresizingMaskIntoConstraints = false
         
         NSLayoutConstraint.activate([
-            persanalDataStackView.topAnchor.constraint(equalTo: topAnchor, constant: 0),
-            persanalDataStackView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 0),
-            persanalDataStackView.trailingAnchor.constraint(equalTo: trailingAnchor, constant: 0),
-            persanalDataStackView.bottomAnchor.constraint(equalTo: bottomAnchor, constant: 0),
+            textFieldStackView.topAnchor.constraint(equalTo: topAnchor, constant: 0),
+            textFieldStackView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 0),
+            textFieldStackView.trailingAnchor.constraint(equalTo: trailingAnchor, constant: 0),
+            textFieldStackView.bottomAnchor.constraint(equalTo: bottomAnchor, constant: 0),
         ])
-
     }
     
     required init?(coder: NSCoder) {
